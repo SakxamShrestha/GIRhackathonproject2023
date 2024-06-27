efd6634ec03c8beb4f4ca600c84cb84b4a1beb12
Sakxam Shrestha <143928022+SakxamShrestha@users.noreply.github.com>
2024-06-26 22:16:46 -0500

    Updated version

--- README.md
+++
@@ -1,2 +0,0 @@
-# GIRhackathonproject2023
-UniSync is a SOS alert web application for students that helps students send SMS to to emergency contacts and 911 with precise location coordinates. 
---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import "./style.css";
+import 'react-toastify/dist/ReactToastify.css';
+import MainPage from "./components/alertpage";
+import LoginPage from "./components/loginpage";
+
+
+
+export default function App() {
+
+    return (
+        <LoginPage/>
+    );
+}
